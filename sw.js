ba0c69f1f60c1eb4cd687f7e064f38c28393d8bb
zombar <jim@8ball.app>
2026-03-18 09:34:04 +0000

    LICENSE file

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'hotnote-bc29caf';
+const CACHE = 'hotnote-d11f4d1';
 const SHELL = ['/', '/index.html', '/manifest.json', '/icon-512.png',
                '/css/style.css', '/js/hotnote.js', '/js/lib-markdown.js', '/js/lib-format.js'];
 
